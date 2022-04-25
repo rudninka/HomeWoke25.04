abe00297496e9ddb483b66fe5ce118b4db27c750
Ekaterina <ekaterina_r21@mail.ru>
2022-04-25 15:51:24 +0300

    Массив

---
+++ Task29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив
+//из 8 элементов и выводит их на экран.
+void fillArray(int[]array, int A, int B)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i]=new Random().Next(A, B);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
+int[] mass = new int[8];
+fillArray(mass, 0, 100);
+PrintArray(mass);
