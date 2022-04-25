3c8bfb3c1f83c7aae706385bf584c201e2374df2
Ekaterina <ekaterina_r21@mail.ru>
2022-04-25 14:43:03 +0300

    А в степени В

---
+++ Task25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход
+//два числа (A и B) и возводит число A в натуральную
+//степень B.
+Console.WriteLine("Введите два числа: ");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int pow = 1;
+
+for(int i = 0; i < B; i++)
+{
+    pow *= A;
+}
+Console.WriteLine(pow);
